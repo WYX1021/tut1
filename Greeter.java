c68380f8d6357d2774a5f9db639f7d6780739de5
Youxin-Wang <w13731191021@163.com>
2022-09-04 16:19:25 +0800

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -21,7 +21,7 @@ public class Greeter {
   }
 
   public static void main(String[] args) {
-    String language = args[0];
+    String language3 = args[0];
     String country = args[1];
     String name = args[2];
     Greeter greeter = new Greeter(language, country, name);
